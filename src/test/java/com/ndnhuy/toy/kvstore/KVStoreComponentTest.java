a4a65f820c263057030128a631c8265ea9dc3ae0
Huy Nguyen <ndnhuy2504@gmail.com>
2024-09-01 20:40:10 +0700

    update

--- src/test/java/com/ndnhuy/toy/kvstore/KVStoreComponentTest.java
+++ src/test/java/com/ndnhuy/toy/kvstore/KVStoreComponentTest.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.UUID;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class KVStoreComponentTest {
@@ -19,14 +21,16 @@ public class KVStoreComponentTest {
     }
 
     void doTestPutGetKeyValueOfInstance(int port) {
-        var url = String.format("http://localhost:%d/kvstore/key1", port);
-        var request = new HttpEntity<>("value1");
+        var key = UUID.randomUUID().toString();
+        var value = UUID.randomUUID().toString();
+        var url = String.format("http://localhost:%d/kvstore/%s", port, key);
+        var request = new HttpEntity<>(value);
         var response = restTemplate.exchange(url, HttpMethod.POST, request, String.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
 
         response = restTemplate.getForEntity(url, String.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(response.getBody()).isEqualTo("value1");
+        assertThat(response.getBody()).isEqualTo(value);
     }
 
 }
---
+++ src/test/java/com/ndnhuy/toy/kvstore/KVStoreIntegrationTest.java
@@ -0,0 +1,12 @@
+package com.ndnhuy.toy.kvstore;
+
+import org.junit.jupiter.api.Test;
+
+public class KVStoreIntegrationTest {
+
+    @Test
+    void testPut_shouldPublishToRabbitMQ() {
+
+    }
+
+}
